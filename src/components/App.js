fe55b85ce46f438ba85c905e4d5f29e56a741407
Anmol53 <anmol.ag53@gmail.com>
2020-12-18 14:41:10 +0530

    Updated App

--- src/components/App.js
+++ src/components/App.js
@@ -1,23 +1,45 @@
-import logo from "./logo.svg";
 import "./App.css";
+import Weather from "./Weather";
+import Search from "./Search";
+import Clock from "./Clock";
+import Calculator from "./Calculator";
+import Modal from "./Modal";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="app">
+      <div className="app-head">
+        <div className="app-nav">
+          <Modal
+            trigger="fas fa-cog"
+            className=""
+            triggerStyle="app-nav-icon"
+            icon
+          />
+          <Modal
+            trigger="Group Tab 1"
+            className=""
+            triggerStyle="app-nav-btn"
+          />
+          <Modal
+            trigger="Group Tab 2"
+            className=""
+            triggerStyle="app-nav-btn"
+          />
+          <Modal
+            trigger="Group Tab 3"
+            className=""
+            triggerStyle="app-nav-btn"
+          />
+          <Calculator className="" triggerStyle="app-nav-btn" />
+        </div>
+        <Clock className="app-clock" />
+        <Weather className="app-weather" />
+      </div>
+      <div className="app-main">
+        <h1>Hey Anmol!</h1>
+        <Search className="app-search" />
+      </div>
     </div>
   );
 }
