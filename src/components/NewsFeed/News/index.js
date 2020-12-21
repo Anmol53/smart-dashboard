477880add4b6e3ded8e007cb326ea64da996d313
Anmol53 <anmol.ag53@gmail.com>
2020-12-21 22:35:41 +0530

    Updated News Feed

--- src/components/NewsFeed/News/index.js
+++ src/components/NewsFeed/News/index.js
@@ -11,9 +11,23 @@ export default function News(props) {
         <div className="news-main">
           <h4 className="news-title">{props.title}</h4>
           <p className="news-content">{props.content}</p>
-          <span className="news-time">{props.time ? props.time : "."}</span>
+          <span className="news-time">
+            {props.time
+              ? new Date(props.time).toLocaleString("en-US", {
+                  weekday: "short",
+                  month: "short",
+                  day: "numeric",
+                  hour: "numeric",
+                  minute: "numeric",
+                })
+              : "."}
+          </span>
           <span className="news-author">
-            {props.author ? props.author : "-"}
+            {props.author
+              ? props.author.length > 30
+                ? props.author.substring(0, 27) + "..."
+                : props.author
+              : "-"}
           </span>
         </div>
       </a>
--- src/components/NewsFeed/index.js
+++ src/components/NewsFeed/index.js
@@ -2,19 +2,20 @@ import React, { useState, useEffect } from "react";
 import "./styles.css";
 import News from "./News";
 
-export default function NewsFeed() {
+export default function NewsFeed(props) {
   const BaseURL = "http://newsapi.org/v2/";
   const APIKey = "c53ce1c2892f4604b64f15aa6b22afbe";
-  const [countryCode, setCountryCode] = useState("in");
   const [newsData, setNewsData] = useState([]);
 
   useEffect(() => {
-    fetch(`${BaseURL}top-headlines?apiKey=${APIKey}&country=${countryCode}`)
+    fetch(
+      `${BaseURL}top-headlines?apiKey=${APIKey}&country=${props.countryCode}`
+    )
       .then((r) => r.json())
       .then((r) => {
         setNewsData(r.articles);
       });
-  }, [countryCode]);
+  }, []);
 
   const renderNews = () => {
     return newsData
